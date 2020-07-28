2bb7ed6cf55f2d7debe4bd573921ca1cad87d4b5
prateekchandra <prateekchandra1027@gmail.com>
2020-07-28 15:35:00 +0530

    Added Ceaser-Cipher

--- ceaser-cipher.py
+++ ceaser-cipher.py
@@ -1,7 +1,8 @@
 alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 string_input= raw_input("Enter a string: ")
-
+shift_input= int(input("Enter in a value to shift by: "))
+shift_input=shift_input%26
 input_length= len(string_input)
 
 
@@ -11,7 +12,7 @@ string_output=""
 for i in range(input_length):
     character= string_input[i]
     location_of_character= alphabet.find(character)
-    new_location = location_of_character+13
+    new_location = location_of_character+shift_input
     
     if new_location >=26:
         new_location-=26
